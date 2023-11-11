662398c8e933fc07d777225ad9e44cb4148b7df2
Larwive <larwive@gmail.com>
2023-11-11 17:57:28 +0100

    Fonctionne.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Assuming your Node.js server is hosted on Heroku
+const apiUrl = 'https://intense-depths-20444-c1e4653348f9.herokuapp.com'; // Replace with your actual Heroku app name
+
+fetch(`${apiUrl}/api/data`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        // Handle the data received from the server
+    })
+    .catch((error) => {
+        console.error('Error fetching data', error);
+    });
+
+
 function App() {
   return (
     <div className="App">
@@ -9,14 +24,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload. Et ouais.
         </p>
-        <a
+        <anpm
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </anpm>
       </header>
     </div>
   );
