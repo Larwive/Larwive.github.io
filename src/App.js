10d42e8dc0e0d5d7fd51a4913593614615aa2284
Larwive <larwive@gmail.com>
2023-11-13 20:42:14 +0100

    Testing Glitch auto update.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+const cors = require('cors');
+const postgres = require('postgres');
+const { Client } = require("pg");
+require('dotenv').config();
+
+const cmd = require("node-cmd"); //Auto github update
+
+const corsOptions = {
+    origin: '*', //'https://larwive.github.io',
+};
+
+// Enable CORS for all routes
+app.use(cors(corsOptions));
+
+app.use(express.json());
 
 
-// Assuming your Node.js server is hosted on Heroku
-const apiUrl = 'https://kindhearted-lava-giver.glitch.me'; // Replace with your actual Heroku app name
+// Database connection
+const client = new Client({
+    user: "fyqtkvlq",
+    host: "flora.db.elephantsql.com",
+    database: "fyqtkvlq",
+    password: "toDYnbZmOfBhVKB7RRE1QUlxzr7I3aBz",
+    port: "5432",
+});
 
-
-const postQuery = {
-    queryType: "SELECT",
-    fetching: "*",
-    table: "FROM constructeurs"
+async function connectToDatabase() {
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL database');
+    } catch (err) {
+        console.error('Error connecting to PostgreSQL:', err);
+    }
 }
 
+connectToDatabase();
 
-fetch(`${apiUrl}/api/data`, {
-    method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(postQuery),
-})
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        // Handle the data received from the server
+// Handle both GET and POST requests
+app.route('/api/data')
+    .get(async (req, res) => {
+        console.log("Here (GET).\n");
+        try {
+            const result = await client.query(
+                "SELECT $1::text as message", [
+                    "Hello world from node.js server ! Hey. brooother",
+                ])
+            ;
+            res.json(result);
+        } catch (err) {
+            console.error('Error executing query (GET).', err);
+            res.status(500).json({ error: 'An error occurred (GET).' });
+        }
     })
-    .catch((error) => {
-        console.error('Error fetching data', error);
+    .post(async (req, res) => {
+
+        const postData = req.body;
+
+        console.log(`${postData.queryType} ${postData.fetching} ${postData.table};\n`);
+
+        try {
+            const result = await client.query(`${postData.queryType} ${postData.fetching} ${postData.table};`);
+            res.json(result);
+        } catch (err) {
+            console.error('Error executing query (POST)', err);
+            res.status(500).json({ error: 'An error occurred (POST)' });
+        }
     });
 
+//Auto github update
+app.post('/git', (req, res) => {
+    // If event is "push"
+    if (req.headers['x-github-event'] == "push") {
+        if (req.headers['x-github-event'] == "push") {
+            cmd.run('chmod 777 git.sh'); /* :/ Fix no perms after updating */
+            cmd.get('./git.sh', (err, data) => {  // Run our script
+                if (data) console.log(data);
+                if (err) console.log(err);
+            });
+            cmd.run('refresh');  // Refresh project
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Et ouais. larwive-requests
-        </p>
-        <anpm
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </anpm>
-      </header>
-    </div>
-  );
-}
+            console.log("> [GIT] Updated with origin/master");
+        }
+    }
 
-export default App;
+    return res.sendStatus(200); // Send back OK status
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
