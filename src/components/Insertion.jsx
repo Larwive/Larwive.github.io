2944af2a939a818e8e5707ec7d1c6d1714d2f672
tdaponte <tanguy.daponte@gmail.com>
2023-12-06 23:32:38 +0100

    multiple fixes and features

--- src/components/Insertion.jsx
+++ src/components/Insertion.jsx
@@ -5,39 +5,33 @@ const baseUrl = 'https://kindhearted-lava-giver.glitch.me';
 
 export const ClientInsertion = () => {
 
+    const initalState = { "id": "admin", "password": "admin", "nom_client": "", "prenom_client": "", "adresse_client": "", "mail_client": "", "telephone_client": "" }
     const [data, setData] = useState({ 'id': "admin", 'password': 'admin' })
 
     return (
         <div className='insertion-client'>
-            <h2 className='insertion-client-title'>Ajoutez un client à la base de donnée</h2>
+            <h2 className='insertion-title'>Ajouter un client à la base de donnée</h2>
             <form className='insertion-client-data'>
-                <input type='text' placeholder='Nom' required="required" onChange={(e) => setData({ ...data, nom_client: e.target.value })} />
-                <input type='text' placeholder='Prenom' required="required" onChange={(e) => setData({ ...data, prenom_client: e.target.value })} />
-                <input type='text' placeholder='Adresse' required="required" onChange={(e) => setData({ ...data, adresse_client: e.target.value })} />
-                <input type='email' placeholder='Email' required="required" onChange={(e) => setData({ ...data, mail_client: e.target.value })} />
-                <input type='tel' placeholder='Téléphone' required="required" onChange={(e) => setData({ ...data, telephone_client: e.target.value })} />
-                <input type='submit' className='instertion-client-add' onClick={(e) => {
-                    e.preventDefault();
-                    fetch(`${baseUrl}/insertion/clients`, {
-                        method: 'POST', headers: {
-                            "Content-Type": "application/json",
-                        }, body: JSON.stringify(data)
-                    })
-                        .then(response => response.json())
-                        .then(data => console.log(data));
-                }}></input>
+                <input type='text' placeholder='Nom' required="required" value={data.nom_client} onChange={(e) => setData({ ...data, nom_client: e.target.value })} />
+                <input type='text' placeholder='Prenom' required="required" value={data.prenom_client} onChange={(e) => setData({ ...data, prenom_client: e.target.value })} />
+                <input type='text' placeholder='Adresse' required="required" value={data.adresse_client} onChange={(e) => setData({ ...data, adresse_client: e.target.value })} />
+                <input type='email' placeholder='Email' required="required" value={data.mail_client} onChange={(e) => setData({ ...data, mail_client: e.target.value })} />
+                <input type='tel' placeholder='Téléphone' required="required" value={data.telephone_client} onChange={(e) => setData({ ...data, telephone_client: e.target.value })} />
+                <ExportButton className='instertion-client-add' route="clients" data={data} initalState={initalState} setData={setData} />
+
             </form>
         </div >
     )
 }
 
 export const VehiculeInsertion = () => {
-    const [data, setData] = useState({ 'id': "admin", 'password': 'admin' })
+    const initalState = { "id": "admin", "password": "admin", "immatriculation": "", "date_de_mise_en_circulation": "", "type_vehicule": "" }
+    const [data, setData] = useState({ "id": "admin", "password": "admin" })
     const [clients, setClients] = useState([])
     const [models, setModels] = useState([])
 
     useEffect(() => {
-        fetch(`${baseUrl}/gettable`, {
+        fetch(`${baseUrl}/search`, {
             method: "POST", headers: {
                 "Content-Type": "application/json",
             }, body: JSON.stringify({ table: "clients" })
@@ -48,7 +42,7 @@ export const VehiculeInsertion = () => {
                 setClients(data.rows.map(elm => { return { id_client: elm.id_client, nom_client: elm.nom_client, prenom_client: elm.prenom_client } }))
             });
 
-        fetch(`${baseUrl}/gettable`, {
+        fetch(`${baseUrl}/search`, {
             method: "POST", headers: {
                 "Content-Type": "application/json",
             }, body: JSON.stringify({ table: "modeles" })
@@ -62,27 +56,18 @@ export const VehiculeInsertion = () => {
 
     return (
         <div className='insertion-vehicule'>
-            <h2 className='insertion-vehicule-title'>Ajoutez un véhciules à la base de donnée</h2>
+            <h2 className='insertion-title'>Ajouter un véhicule à la base de donnée</h2>
             <form className='insertion-vehicule-data'>
-                <input type='text' placeholder='Immatriculation' onChange={(e) => setData({ ...data, immatriculation: e.target.value })} />
-                <input type='date' placeholder='Date de mise en circulation' onChange={(e) => setData({ ...data, date_de_mise_en_circulation: e.target.value })} />
-                <input type='text' placeholder='Type de véhicule' onChange={(e) => setData({ ...data, type_vehicule: e.target.value })} />
+                <input type='text' placeholder='Immatriculation' value={data.immatriculation} onChange={(e) => setData({ ...data, immatriculation: e.target.value })} />
+                <input type='date' placeholder='Date de mise en circulation' value={data.date_de_mise_en_circulation} onChange={(e) => setData({ ...data, date_de_mise_en_circulation: e.target.value })} />
+                <input type='text' placeholder='Type de véhicule' value={data.type_vehicule} onChange={(e) => setData({ ...data, type_vehicule: e.target.value })} />
                 <select type='text' placeholder='Client' onChange={(e) => setData({ ...data, id_client: JSON.parse(e.target.value).id_client })} >
                     {clients.map(client => <option>{JSON.stringify(client)}</option>)}
                 </select>
                 <select type='text' placeholder='Modèle' onChange={(e) => setData({ ...data, id_modele: JSON.parse(e.target.value).id_modele })} >
                     {models.map(model => <option>{JSON.stringify(model)}</option>)}
                 </select>
-                <input type='submit' className='instertion-client-add' onClick={(e) => {
-                    e.preventDefault(); // TODO: factoriser cette merde
-                    fetch(`${baseUrl}/insertion/vehicules`, {
-                        method: 'POST', headers: {
-                            "Content-Type": "application/json",
-                        }, body: JSON.stringify(data)
-                    })
-                        .then(response => response.json())
-                        .then(data => console.log(data));
-                }}></input>
+                <ExportButton className='instertion-vehicules-add' route="vehicules" data={data} initalState={initalState} setData={setData} />
             </form>
         </div>
 
@@ -92,12 +77,13 @@ export const VehiculeInsertion = () => {
 
 export const BillInsertion = () => {
 
-    const [data, setData] = useState({ 'id': "admin", 'password': 'admin' })
+    const initalState = { "id": "admin", "password": "admin", "montant": "", "date_facture": "" }
+    const [data, setData] = useState(initalState)
     const [clients, setClients] = useState([])
     const [interventions, setInterventions] = useState([])
 
     useEffect(() => {
-        fetch(`${baseUrl}/gettable`, {
+        fetch(`${baseUrl}/search`, {
             method: "POST", headers: {
                 "Content-Type": "application/json",
             }, body: JSON.stringify({ table: "clients" })
@@ -108,7 +94,7 @@ export const BillInsertion = () => {
                 setClients(data.rows.map(elm => { return { id_client: elm.id_client, nom_client: elm.nom_client, prenom_client: elm.prenom_client } }))
             });
 
-        fetch(`${baseUrl}/gettable`, {
+        fetch(`${baseUrl}/search`, {
             method: "POST", headers: {
                 "Content-Type": "application/json",
             }, body: JSON.stringify({ table: "interventions" })
@@ -121,30 +107,35 @@ export const BillInsertion = () => {
 
     return (
         <div className='insertion-bill'>
-            <h2 className='insertion-bill-title'>Ajoutez une facture</h2>
+            <h2 className='insertion-title'>Ajouter une facture</h2>
             <form className='insertion-bill-data'>
-                <input type='text' placeholder='Montant' required="required" onChange={(e) => setData({ ...data, montant: e.target.value })} />
-                <input type='date' placeholder='Date Facture' required="required" onChange={(e) => setData({ ...data, date_facture: e.target.value })} />
+                <input type='text' placeholder='Montant' required="required" value={data.montant} onChange={(e) => setData({ ...data, montant: e.target.value })} />
+                <input type='date' placeholder='Date Facture' required="required" value={data.date_facture} onChange={(e) => setData({ ...data, date_facture: e.target.value })} />
                 <select onChange={(e) => setData({ ...data, id_client: JSON.parse(e.target.value).id_client })} >
                     {clients.map(client => <option>{JSON.stringify(client)}</option>)}
                 </select>
                 <select onChange={(e) => setData({ ...data, id_intervention: JSON.parse(e.target.value).id_intervention })} >
                     {interventions.map(intervention => <option>{JSON.stringify(intervention)}</option>)}
                 </select>
-                <input type='submit' className='instertion-bill-add' onClick={(e) => {
-                    e.preventDefault();
-                    console.log(data)
-                    fetch(`${baseUrl}/insertion/factures`, {
-                        method: 'POST', headers: {
-                            "Content-Type": "application/json",
-                        }, body: JSON.stringify(data)
-                    })
-                        .then(response => response.json())
-                        .then(data => console.log(data));
-                }}></input>
+                <ExportButton className='instertion-bill-add' route="factures" data={data} initalState={initalState} setData={setData} />
             </form>
         </div >
     )
+}
+
+const ExportButton = ({ data, route, initalState, setData }) => {
+    return <input type='submit' onClick={(e) => {
+        e.preventDefault();
+        console.log(data)
+        fetch(`${baseUrl}/insertion/${route}`, {
+            method: 'POST', headers: {
+                "Content-Type": "application/json",
+            }, body: JSON.stringify(data)
+        })
+            .then(response => response.json())
+            .then(data => console.log(data));
+        setData(initalState)
+    }}></input>
 }
 
 
--- src/components/RequestPannel.jsx
+++ src/components/RequestPannel.jsx
@@ -1,60 +1,58 @@
 import React, { useState } from 'react'
 import './RequestPannel.css'
 
-export const DemoRequestPannel = () => {
-
-    const [data, setData] = useState(null)
-
-    return (
-        <div className='request-pannel'>
-            <div className='request-buttons'>
-                <RequestButton name='Clients' url='https://kindhearted-lava-giver.glitch.me/gettable' setData={setData} />
-                <RequestButton name='Interventions' url='https://kindhearted-lava-giver.glitch.me/intervention' setData={setData} />
-                {/* <RequestButton name='REQ 3' url='https://kindhearted-lava-giver.glitch.me/gettable' setData={setData} />
-                <RequestButton name='REQ 4' url='https://kindhearted-lava-giver.glitch.me/gettable' setData={setData} />
-                <RequestButton name='REQ 5' url='' setData={setData} /> */}
-                {/* <RequestButton name='REQ 6' url='' setData={setData} /> */}
-            </div>
-            <div className='request-result'>{data === null ? <div></div> :
-                <div style={{ display: 'grid', gridTemplateColumns: `repeat(${Object.keys(data[0]).length}, auto)`, gap: '20px' }}>
-                    {Object.keys(data[0]).map(cat => <div className='request-categorie'>{cat}</div>)}
-                    {data.map((elm) => Object.values(elm).map(val => <div className='request-value' key={val}>{val}</div>))}
-                </div>}
-            </div>
-        </div>
-    )
-}
-
 export const RequestPannel = () => {
 
+
     const [data, setData] = useState(null);
-    const [options, setOptions] = useState({});
+    const [categorie, setCategorie] = useState("")
+    const [option, setOption] = useState({})
+
+    function showInputs(cat) {
+        switch (cat) {
+            case "Clients":
+                return <div className='inputs'>
+                    <ClientOption option={option} setOption={setOption} />
+                    <SpecialRequest option={option} setData={setData} route="entrustedVehicules" name="Véhicule/Personne" />
+                    <SpecialRequest option={option} setData={setData} route="invoicesSum" name="Montant/Personne" />
+                    <RequestButton option={option} setOption={setOption} setData={setData} route="clients" />
+                </div>
+            case "Véhicules":
+                return <div className='inputs'>
+
+                    <VehiculeOption option={option} setOption={setOption} />
+                    <SpecialRequest option={option} setData={setData} route="listModelsInterval" name="Liste modèles" />
+                    <RequestButton option={option} setOption={setOption} setData={setData} route="vehicules" />
+                </div>
+            case "Factures":
+                return <div className='inputs'>
+                    <FactureOption option={option} setOption={setOption} />
+                    <RequestButton option={option} setOption={setOption} setData={setData} route="factures" />
+                </div>
+            // case "Interventions":
+            //     return <div className='inputs'>
+            //         <InterventionOption option={option} setOption={setOption} />
+            //         <RequestButton option={option} setOption={setOption} setData={setData} route="intervention" />
+            //     </div>
+            default:
+                return <div className='inputs'>
+                    <ClientOption option={option} setOption={setOption} />
+                    <SpecialRequest option={option} setData={setData} route="entrustedVehicules" name="Véhicule/Personne" />
+                    <SpecialRequest option={option} setData={setData} route="invoicesSum" name="Montant/Personne" />
+                    <RequestButton option={option} setOption={setOption} setData={setData} route="clients" />
+                </div>
+        }
+    }
 
     return (
         <div className='request-pannel'>
-            <div className='request-buttons'>
-                {/* <input className='request-name-input' type='text' onChange={(e) => setOptions({ ...options, nom: e.target.value })} />
-                <input className='request-name-input' type='text' onChange={(e) => setOptions({ ...options, prenom: e.target.value })} /> */}
-                <RequestButton name='Clients' url='https://kindhearted-lava-giver.glitch.me/search' setData={setData} option={{
-                    method: 'POST', headers: {
-                        "Content-Type": "application/json",
-                    }, body: JSON.stringify({ ...options, table: 'clients' })
-                }} />
-                <RequestButton name='Vehicules' url='https://kindhearted-lava-giver.glitch.me/search' setData={setData} option={{
-                    method: 'POST', headers: {
-                        "Content-Type": "application/json",
-                    }, body: JSON.stringify({ ...options, table: 'vehicules' })
-                }} />
-                <RequestButton name='Interventions' url='https://kindhearted-lava-giver.glitch.me/search' setData={setData} option={{
-                    method: 'POST', headers: {
-                        "Content-Type": "application/json",
-                    }, body: JSON.stringify({ ...options, table: 'interventions' })
-                }} />
-                <RequestButton name='Factures' url='https://kindhearted-lava-giver.glitch.me/search' setData={setData} option={{
-                    method: 'POST', headers: {
-                        "Content-Type": "application/json",
-                    }, body: JSON.stringify({ ...options, table: 'factures' })
-                }} />
+            <div>
+                <select className='request-categorie' value={categorie} onChange={(e) => setCategorie(e.target.value)}>
+                    <option>Clients</option>
+                    <option>Véhicules</option>
+                    <option>Factures</option>
+                </select>
+                {showInputs(categorie)}
             </div>
             <div className='request-result'>{data === null ? <div>Loading...</div> :
                 <div style={{ display: 'grid', gridTemplateColumns: `repeat(${Object.keys(data[0]).length}, auto)`, gap: '20px' }}>
@@ -66,16 +64,92 @@ export const RequestPannel = () => {
     )
 }
 
-const RequestButton = ({ name, url, setData, option }) => {
+const RequestButton = ({ option, setData, route }) => {
+
+    const url = 'https://kindhearted-lava-giver.glitch.me/search'
+
     return <button className='request-button'
-        onClick={() => {
+        onClick={(e) => {
+            e.preventDefault()
             setData(null);
-            fetch(url, option)
+            fetch(url, {
+                method: 'POST', headers: {
+                    "Content-Type": "application/json",
+                }, body: JSON.stringify({ ...option, table: route })
+            })
                 .then(response => response.json())
-                .then((data) => setData(data.rows));
+                .then(data => {
+                    console.log(JSON.stringify(data))
+                    if (data.count > 0) {
+                        setData(data.rows)
+                    }
+                });
+        }}>
+        <h2>Chercher</h2>
+    </button>
+}
+
+function handleInputChange(e, key, option, setOption) {
+    e.preventDefault()
+    const value = e.target.value
+    if (value.length > 0) {
+        const newOption = { ...option }
+        newOption[key] = value;
+        setOption(newOption)
+    }
+    else {
+        delete option[key]
+        setOption({ ...option })
+    }
+}
+
+
+const ClientOption = ({ option, setOption }) => {
+
+    return <form className='insertion'>
+        <input type='text' placeholder='Nom' onChange={(e) => handleInputChange(e, "nom_client", option, setOption)} />
+        <input type='text' placeholder='Prenom' onChange={(e) => handleInputChange(e, "prenom_client", option, setOption)} />
+        <input type='text' placeholder='Adresse' onChange={(e) => handleInputChange(e, "adresse_client", option, setOption)} />
+        <input type='email' placeholder='Email' onChange={(e) => handleInputChange(e, "mail_client", option, setOption)} />
+        <input type='tel' placeholder='Téléphone' onChange={(e) => handleInputChange(e, "telephone_client", option, setOption)} />
+    </form>
+}
+
+const FactureOption = ({ option, setOption }) => {
+    return <form className='insertion'>
+        <input type='text' placeholder='Montant' onChange={(e) => handleInputChange(e, "montant", option, setOption)} />
+        <input type='date' placeholder='Date Facture' onChange={(e) => handleInputChange(e, "date_facture", option, setOption)} />
+    </form>
+}
+const VehiculeOption = ({ option, setOption }) => {
+    return <form className='insertion'>
+        <input type='text' placeholder='Immatriculation' onChange={(e) => handleInputChange(e, "immatriculation", option, setOption)} />
+        <input type='date' placeholder='Date de mise en circulation' onChange={(e) => handleInputChange(e, "date_de_mise_en_circulation", option, setOption)} />
+        <input type='text' placeholder='Type de véhicule' onChange={(e) => handleInputChange(e, "type_vehicule", option, setOption)} />
+    </form>
+}
+
+const SpecialRequest = ({ option, setData, route, name }) => {
+
+    const baseUrl = 'https://kindhearted-lava-giver.glitch.me/'
+
+    return <button className='request-button'
+        onClick={(e) => {
+            e.preventDefault()
+            setData(null);
+            fetch(`${baseUrl}/${route}`, {
+                method: 'POST', headers: {
+                    "Content-Type": "application/json",
+                }, body: JSON.stringify({ ...option })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    setData(data.rows)
+                });
         }}>
         <h2>{name}</h2>
     </button>
 }
+
 
 export default RequestPannel
